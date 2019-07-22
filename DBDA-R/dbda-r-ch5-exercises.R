d50a152477d8096dd74d46f7c03db73cd1c21272
jamescrosbie <jamescrosbie@hotmail.co.uk>
2019-07-22 15:17:37 +0100

    Some ch5 questions

--- DBDA-R/dbda-r-ch5-exercises.R
+++ DBDA-R/dbda-r-ch5-exercises.R
@@ -31,3 +31,30 @@ posterior <- BernBeta(prior, data)
 
 data <- rep(c(1,0), c(57,43))
 posterior <- BernBeta(posterior, data)
+
+#5.5
+#strong prior beliefs as minted coin
+prior <- c(100,100)
+data <- rep(c(0,1), c(1,9))
+posterior <- BernBeta(prior, data)
+# p(h|history) ~ 0.52 maximum of posterior
+
+#strong prior beliefs that unfair, but dont know which way
+prior <- c(0.1,0.1)
+data <- rep(c(0,1), c(1,9))
+posterior <- BernBeta(prior, data)
+# p(h|history) ~ 0.85 maximum of posterior
+
+#5.6
+prior1 <- c(10,10)
+data <- rep(c(0,1), c(5,15))
+posterior1 <- BernBeta(prior1, data)
+evid1 <- 2.45*10^-6
+
+prior2 <- c(0.1,0.1)
+posterior2 <- BernBeta(prior2, data)
+evid2 <- 7.32*10^-7
+
+#Evidence 2 is samller, therefore suggests model 1 is more likely
+print(paste("Relative in favour of model 2: ", round(evid2/evid1,2)))
+
